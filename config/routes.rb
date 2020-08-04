1af67631bffded90235a2b38fa3b672a0e5ff127
muromoto-y <6610010@gmail.com>
2020-08-04 18:05:42 +0900

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # root to: 'posts#index'
   # get 'posts', to: 'posts#index'
-  # get 'posts/new', to: 'posts#new'
   # post 'posts',to: 'posts#create'
   root to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
   get 'posts/:id', to: 'posts#checked'
  end
